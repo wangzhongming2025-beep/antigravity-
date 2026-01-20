f9408fb6a9005edb3079e7ccd3613ef0c92a7c86
laowangduizhang <1039469321@qq.com>
2026-01-20 10:28:07 +0800

    fix: redirect index.html to root

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/index.html',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
